3c032eb908da9275765a4a7d8496ad24927c0552
codenoidea <codenoidea@gmail.com>
2024-05-30 17:20:01 +0900

    docker-compose

--- gateway/app.ts
+++ gateway/app.ts
@@ -44,8 +44,8 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 
   // Bind connection to error event (to get notification of connection errors)
-  // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  // db.once('open', () => {
-  //   console.log('mongo db connection OK.');
-  // });
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  db.once('open', () => {
+    console.log('mongo db connection OK.');
+  });
 })
